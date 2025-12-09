18a548b5c8ce5af2768407bfd4b6bfd7ad1bd7fa
IllyaShramko <123illya123123r@gmail.com>
2025-12-09 18:15:41 +0200

    Added category module

---
+++ src/Category/category.routes.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import { CategoryWithProducts } from './category.controller'
+
+const CategoryRouter: express.Router = express.Router()
+
+
+CategoryRouter.get("/", CategoryWithProducts.getAll);
+
+CategoryRouter.get("/:id", CategoryWithProducts.getById);
+
+CategoryRouter.post("/", CategoryWithProducts.create);
+
+CategoryRouter.put("/:id", CategoryWithProducts.update)
+
+CategoryRouter.delete("/:id", CategoryWithProducts.delete)
+export { CategoryRouter }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { ProductRouter } from './Product/product.routes'
+import { CategoryRouter } from './Category/category.routes'
 
 const app: express.Express = express()
 app.use(express.json()); 
 app.use("/products/", ProductRouter)
+app.use("/categories/", CategoryRouter)
 
 
 const PORT = 8000
